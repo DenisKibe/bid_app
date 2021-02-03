b1b78f324e9fbda2a32e96b4a11ba28389de608f
DenisKibe <kibedenis@gmail.com>
2021-02-04 00:02:33 +0300

    add

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
 class Config(object):
-    SECRET_KEY=""
-    SQLALCHEMY_DATABASE_URL='postgresql://kibe:denis@localhost/bid_app'
+    SECRET_KEY="\xac\x90\x8bi\x89z\x12P\xb2\x9c\x8cg>\xeaU\x1d\xb5#\xa0\xb8\x08\x9d\xcd\xc9\xc7\x9e1D\xdb;\xe6\xf1]\x8bM'Mc\x80pX#Oo\xb9k0X*\x8a\xcf}\xb2V\x96\xc3\x97!/\x1d\xd4\xf5\x9a\xf7"
+    SQLALCHEMY_DATABASE_URI='postgresql://kibe:denis@localhost/bid_app'
     CSRF_ENABLED=True
     DEBUG = False
     UPLOAD_FOLDER='app/statict/uploads/'
